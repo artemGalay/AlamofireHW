902b6c39306e98716463833f3c19d7ea931c861b
Artem Galay <iostema@mail.ru>
2022-11-02 09:36:59 +0300

    Initial Commit

---
+++ AlamofireHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireHW
+//
+//  Created by Артем Галай on 2.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
